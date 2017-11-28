4fe2072a62cae87b1ac1439a7bb9e10b35d3835d
BenTheGod <bentolima100@gmail.com>
2017-11-28 18:10:41 -0100

    madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,12 @@
+
+
+
+import sys, shelve
+
+word = ['ADJECTIVE','NOUN','ADVERB','VERB']
+
+lib_file = read('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt')
+
+for word in lib_file.split():
+	if word.lower() in lib_file.split:
+		newlibfile

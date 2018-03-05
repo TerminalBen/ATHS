ab9e8ca71efd06468761693abda5eeec4d9ad6e6
BenTheGod <bentolima100@gmail.com>
2018-03-05 16:40:26 -0100

    ?
    ?

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -2,11 +2,29 @@
 
 
 import sys, shelve
+'''
+list = ['ADJECTIVE','NOUN','ADVERB','VERB']
+newmadlib=[]
+lib_file = open('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt','r')
 
-word = ['ADJECTIVE','NOUN','ADVERB','VERB']
 
-lib_file = read('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt')
+for word ,index in enumerate (lib_file.split()):
+	if word.lower()in list:
+		newmadlib[index] = input("enter a "+word.upper())
 
-for word in lib_file.split():
-	if word.lower() in lib_file.split:
-		newlibfile
+newmadlib = ' '.join(newmadlib)
+
+print (lib_file)
+print (newmadlib)
+'''
+
+PARTS_OF_SPEECH = ["verb", "noun", "adjective"]
+with open('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt') as madLibFileObject:
+     madLib = madLibFileObject.read()
+newMadLib = []
+for word, index in enumerate(madLib.split()):
+     if word.lower() in PARTS_OF_SPEECH:
+        newMadLib[index] = input("Enter " + word.upper())
+newMadLib = ' '.join(newMadLib)
+print(madLib)
+print(newMadLib)

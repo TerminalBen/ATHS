dddff1a83a3860868ba9eadadceb60d159dde2a8
BenTheGod <bentolima100@gmail.com>
2018-03-16 18:16:50 -0100

    count letter exercice

---
+++ countletter.py
@@ -0,0 +1,8 @@
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for i in message:
+	count.setdefault(i,0)
+	count[i] = count[i] +1
+#print(count)
+for k,v in count.items():
+	print (k,v)

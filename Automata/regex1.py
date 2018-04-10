1b71c2d093b72b6cdb11e4e17f39baa0114f0c01
masterbento <bentolima100@gmail.com>
2018-04-10 14:51:32 -0100

    cont regexes

--- Automata/regex1.py
+++ Automata/regex1.py
@@ -9,3 +9,10 @@ phoneregex = re.compile(r'((\+)?)(\d{3}|\(\d{3}\))(\s|-)(\d{7})')
 emailregex = re.compile (r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9._%+-]+(\.[a-zA-z]{2,4}))')
                             #anything (1 or+) @ anything(1 or+) dot 2or 4 letters
 dateRegex = re.compile (r'((\d{1,31})(\.|\\|\/|\-)(\d{1,12})(\.|\\|\/|\-)\d{4})')
+                            #1-31 ./\- 1-12 ./\- dddd
+
+input = str(pyperclip.paste())
+matches = []
+for group in phoneregex.findall(input):
+    phonenum = '-'.join(group[1],group[2],group[3])
+    

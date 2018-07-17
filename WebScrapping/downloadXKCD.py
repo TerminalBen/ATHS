24758e48a7d76738b29de2976e331bdd66a7fa2d
Ben <bentolima100@gmail.com>
2018-07-17 18:08:21 -0100

    minior tweaks

--- WebScrapping/downloadXKCD.py
+++ WebScrapping/downloadXKCD.py
@@ -8,7 +8,7 @@ path = '/Users/bentolima/Documents/comics'
 os.makedirs(path,exist_ok = True)
 
 while not url.endswith('#'):
-    print('Downloaing page %s...' % url)
+    print('Downloading page %s' % url)
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text,'html.parser')
@@ -18,7 +18,7 @@ while not url.endswith('#'):
         print('commic not found \n')
     else:
         comicurl = 'https:'+comicElem[0].get('src')
-        print('Downloading image... %s' % (comicurl))
+        print('Downloading image %s' % (comicurl))
         res = requests.get(comicurl)
         res.raise_for_status()
         imgFile = open(os.path.join(path,os.path.basename(comicurl)),'wb')
@@ -27,8 +27,8 @@ while not url.endswith('#'):
             imgFile.write(chunk)
         imgFile.close()
 
-#getprevious link
-prevLink = soup.select('a[rel="prev"]')[0]
-url='https://xqcd.com'+prevLink.get('href')
+    #getprevious link
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url='https://xqcd.com'+prevLink.get('href')
 
 print(done)

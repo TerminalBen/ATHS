fb0cda69aab6bc2dd14b3cb11cc1bbd71e1b0400
masterbento <bentolima100@gmail.com>
2019-01-17 00:38:37 -0100

    colecting garbage

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -2,21 +2,6 @@
 
 
 import sys, shelve
-'''
-list = ['ADJECTIVE','NOUN','ADVERB','VERB']
-newmadlib=[]
-lib_file = open('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt','r')
-
-
-for word ,index in enumerate (lib_file.split()):
-	if word.lower()in list:
-		newmadlib[index] = input("enter a "+word.upper())
-
-newmadlib = ' '.join(newmadlib)
-
-print (lib_file)
-print (newmadlib)
-'''
 
 PARTS_OF_SPEECH = ["verb", "noun", "adjective"]
 with open('/Users/bentolima/Documents/Projects/ATHS/Madlibs/madlib.txt') as madLibFileObject:
